17001e56079c5ab762bd29274690ace4bae692c9
Dariusz Kozłowski <dariusz.kozlowski1997@gmail.com>
2021-09-19 17:27:51 +0200

    dbo.Clubs data updating & dbo.Players table creating

--- Ekstraklasa-Database-Queries.sql
+++ Ekstraklasa-Database-Queries.sql
@@ -33,3 +33,76 @@ INSERT INTO dbo.Clubs VALUES
 ('WIS', 'Wis³a Kraków', 'Kraków', 'ma³opolskie', 'Stadion Miejski im. Henryka Reymana', 33130),
 ('WP£', 'Wis³a P³ock', 'P³ock', 'mazowieckie', 'Stadion im. Kazimierza Górskiego', 10978),
 ('ZAG', 'Zag³êbie Lubin', 'Lubin', 'dolnoœl¹skie', 'Stadion Zag³êbia Lubin', 16100);
+
+-- Stadiums name or capacity update
+UPDATE dbo.Clubs
+SET stadium_capacity = 44308
+WHERE locality = 'Wroc³aw';
+
+UPDATE dbo.Clubs
+SET stadium_name = 'Polsat Plus Arena Gdañsk'
+WHERE locality = 'Gdañsk';
+
+UPDATE dbo.Clubs
+SET stadium_capacity = 43269
+WHERE locality = 'Poznañ' AND stadium_name = 'Stadion Poznañ';
+
+UPDATE dbo.Clubs
+SET stadium_capacity = 33326
+WHERE name = 'Wis³a Kraków';
+
+UPDATE dbo.Clubs
+SET stadium_capacity = 31103
+WHERE name = 'Legia Warszawa';
+
+UPDATE dbo.Clubs
+SET stadium_name = 'Stadion im. Ernesta Pohla w Zabrzu', stadium_capacity = 31871
+WHERE locality = 'Zabrze';
+
+UPDATE dbo.Clubs
+SET stadium_capacity = 22386
+WHERE locality = 'Bia³ystok';
+
+UPDATE dbo.Clubs
+SET stadium_capacity = 18023
+WHERE locality = 'Szczecin';
+
+UPDATE dbo.Clubs
+SET stadium_capacity = 16086
+WHERE locality = 'Lubin';
+
+UPDATE dbo.Clubs
+SET stadium_capacity = 10037
+WHERE locality = 'Gliwice';
+
+UPDATE dbo.Clubs
+SET stadium_capacity = 7496
+WHERE locality = '£êczna';
+
+UPDATE dbo.Clubs
+SET stadium_capacity = 7000
+WHERE locality = 'Mielec';
+
+UPDATE dbo.Clubs
+SET stadium_capacity = 6000
+WHERE name = 'Warta Poznañ';
+
+UPDATE dbo.Clubs
+SET stadium_name = 'Miejski Stadion Pi³karski Raków', stadium_capacity = 5500
+WHERE locality = 'Czêstochowa';
+
+CREATE TABLE Players
+(
+	playerID varchar(6) NOT NULL CONSTRAINT key_players PRIMARY KEY,
+	clubID varchar(3) NOT NULL CONSTRAINT fkey_club_players FOREIGN KEY REFERENCES dbo.Clubs(clubID),
+	first_name varchar(20) NOT NULL,
+	last_name varchar(30) NOT NULL,
+	birth_date date NOT NULL,
+	nationality varchar(30) NOT NULL,
+	age smallint,
+	contract_validity date,
+	nominal_position varchar(80) NOT NULL
+);
+
+
+
